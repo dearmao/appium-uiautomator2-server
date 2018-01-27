d5413b03fa6517df3bb99b2fcd426f39b3ab70a6
Jonathan Lipps <jlipps@gmail.com>
2018-01-26 18:39:35 -0800

    make session a private field

--- app/src/main/java/io/appium/uiautomator2/handler/GetSessionDetails.java
+++ app/src/main/java/io/appium/uiautomator2/handler/GetSessionDetails.java
@@ -10,33 +10,37 @@ import io.appium.uiautomator2.http.AppiumResponse;
 import io.appium.uiautomator2.http.IHttpRequest;
 import io.appium.uiautomator2.model.AccessibilityScrollData;
 import io.appium.uiautomator2.model.AppiumUiAutomatorDriver;
+import io.appium.uiautomator2.model.Session;
 import io.appium.uiautomator2.server.WDStatus;
 import io.appium.uiautomator2.utils.Logger;
 
 public class GetSessionDetails extends SafeRequestHandler {
 
-        public GetSessionDetails(String mappedUri) {
-            super(mappedUri);
-        }
+    private Session session;
 
-        @Override
-        public AppiumResponse safeHandle(IHttpRequest request) {
-            try {
-                JSONObject result = new JSONObject();
-                AccessibilityScrollData scrollData = AppiumUiAutomatorDriver.getInstance().getSession().getLastScrollData();
-                HashMap<String, Integer> scrollDataMap;
-                if (scrollData == null) {
-                    scrollDataMap = null;
-                } else {
-                    scrollDataMap = scrollData.getAsMap();
-                }
-                JSONObject lastScrollData = new JSONObject(scrollDataMap);
-                result.put("lastScrollData", lastScrollData);
-                return new AppiumResponse(getSessionId(request), WDStatus.SUCCESS, result);
-            } catch (JSONException e) {
-                Logger.error("Exception while reading JSON: ", e);
-                Logger.error(WDStatus.JSON_DECODER_ERROR, e);
-                return new AppiumResponse(getSessionId(request), WDStatus.JSON_DECODER_ERROR, e);
+    public GetSessionDetails(String mappedUri) {
+        super(mappedUri);
+        session = AppiumUiAutomatorDriver.getInstance().getSession();
+    }
+
+    @Override
+    public AppiumResponse safeHandle(IHttpRequest request) {
+        try {
+            JSONObject result = new JSONObject();
+            AccessibilityScrollData scrollData = session.getLastScrollData();
+            HashMap<String, Integer> scrollDataMap;
+            if (scrollData == null) {
+                scrollDataMap = null;
+            } else {
+                scrollDataMap = scrollData.getAsMap();
             }
+            JSONObject lastScrollData = new JSONObject(scrollDataMap);
+            result.put("lastScrollData", lastScrollData);
+            return new AppiumResponse(getSessionId(request), WDStatus.SUCCESS, result);
+        } catch (JSONException e) {
+            Logger.error("Exception while reading JSON: ", e);
+            Logger.error(WDStatus.JSON_DECODER_ERROR, e);
+            return new AppiumResponse(getSessionId(request), WDStatus.JSON_DECODER_ERROR, e);
         }
+    }
 }
