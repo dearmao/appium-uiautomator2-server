ef21b2fd6ae8f157361f994179036cad72177eb3
Jonathan Lipps <jlipps@gmail.com>
2018-01-29 20:43:06 -0800

    don't use executeAndWaitForEvent, because it returns random events instead listen to the stream of events so we're sure we get the last one

--- app/src/main/java/io/appium/uiautomator2/core/EventRegister.java
+++ app/src/main/java/io/appium/uiautomator2/core/EventRegister.java
@@ -5,6 +5,7 @@ import android.app.UiAutomation;
 import android.support.test.uiautomator.Configurator;
 import android.view.accessibility.AccessibilityEvent;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeoutException;
 
 import io.appium.uiautomator2.model.AccessibilityScrollData;
@@ -15,39 +16,54 @@ import io.appium.uiautomator2.utils.Logger;
 
 public abstract class EventRegister {
 
+    public static int EVENT_COOLDOWN_MS = 750;
+
     public static Boolean runAndRegisterScrollEvents (ReturningRunnable<Boolean> runnable, long timeout) {
         // turn off listening to notifications since it interferes with us listening for the scroll
         // event here
         NotificationListener listener = NotificationListener.getInstance();
+
         boolean notificationListenerActive = listener.isListening;
         if (notificationListenerActive) {
             listener.stop();
         }
 
-        // say we want to listen to only scroll events
-        UiAutomation.AccessibilityEventFilter eventFilter = new UiAutomation.AccessibilityEventFilter() {
+        // here we set a callback for the accessibility event stream, keeping track of any scroll
+        // events we come across
+        AccessibilityEvent event = null;
+        final ArrayList<AccessibilityEvent> seenEvents = new ArrayList<>();
+        UiAutomation automation = UiAutomatorBridge.getInstance().getUiAutomation();
+        UiAutomation.OnAccessibilityEventListener onEvent = new UiAutomation.OnAccessibilityEventListener() {
             @Override
-            public boolean accept(AccessibilityEvent event) {
-                return event.getEventType() == AccessibilityEvent.TYPE_VIEW_SCROLLED;
+            public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+                if (accessibilityEvent.getEventType() == AccessibilityEvent.TYPE_VIEW_SCROLLED) {
+                    seenEvents.add(accessibilityEvent);
+                }
             }
         };
+        automation.setOnAccessibilityEventListener(onEvent);
 
-        AccessibilityEvent event = null;
-        try {
-            event = UiAutomatorBridge.getInstance().getUiAutomation().executeAndWaitForEvent(runnable,
-                    eventFilter, timeout);
-            Logger.debug("Retrieved accessibility event for scroll");
-        } catch (TimeoutException ignore) {
-            Logger.error("Expected to receive a scroll accessibility event but hit the timeout instead");
+        // actually run the swipe/scroll
+        runnable.run();
+
+        // wait a certain amount of time for any accessibility events generated to be caught by
+        // our callback, then stop watching for events
+        try { Thread.sleep(timeout); } catch (InterruptedException ign) {}
+        automation.setOnAccessibilityEventListener(null);
+
+        // if we have caught any events in our net, snatch the last one
+        if (seenEvents.size() > 0) {
+            event = seenEvents.get(seenEvents.size() - 1); // get the last event we saw
         }
 
         Session session = AppiumUiAutomatorDriver.getInstance().getSession();
 
         if (event == null) {
+            Logger.debug("Did not retrieve accessibility event for scroll");
             session.setLastScrollData(null);
         } else {
             AccessibilityScrollData data = new AccessibilityScrollData(event);
-            Logger.debug("Accessibility event was: ", data);
+            Logger.debug("Retrieved accessibility event for scroll: ", data);
             session.setLastScrollData(data);
         }
 
@@ -61,6 +77,6 @@ public abstract class EventRegister {
     }
 
     public static Boolean runAndRegisterScrollEvents (ReturningRunnable<Boolean> runnable) {
-        return runAndRegisterScrollEvents(runnable, Configurator.getInstance().getScrollAcknowledgmentTimeout());
+        return runAndRegisterScrollEvents(runnable, EVENT_COOLDOWN_MS);
     }
 }
